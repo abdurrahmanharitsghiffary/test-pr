761eb16e5e51bece617ed5c26c388a4b431da3aa
dangdang2121 <haffae124@gmail.com>
2024-07-05 03:14:16 +0700

    Edit server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const loler = "HALLO";
 const sup = "SUPEREM";
 
-const hello = () => {
+const getServerPooling = () => {
   const serverPooling = 1000;
   return serverPooling;
 };
 
 const HELLO = "WORLD";
+getServerPooling();
