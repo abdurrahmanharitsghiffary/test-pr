2c9b829c3f304647c07d3dd3d0ced38734e396ff
dangdang2121 <haffae124@gmail.com>
2024-07-05 03:17:16 +0700

    Wrong commit

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const loler = "HALLO";
 const sup = "SUPEREM";
 
+const wrongServerPooling = () => {
+  return getServerPooling() - getServerPooling();
+};
+
 const getServerPooling = () => {
   const serverPooling = 1000;
   return serverPooling;
 };
 
 const HELLO = "WORLD";
-getServerPooling();
+wrongServerPooling();
