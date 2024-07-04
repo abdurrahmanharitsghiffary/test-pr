5c9ca30b60161ca73ce9428890c46eedb9c2aec8
dangdang2121 <haffae124@gmail.com>
2024-07-05 03:12:17 +0700

    correct rename

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const loler = "HALLO";
 const sup = "SUPEREM";
 
 const hello = () => {
-  const poolor = 1000;
-  return poolor;
+  const serverPooling = 1000;
+  return serverPooling;
 };
 
 const HELLO = "WORLD";
